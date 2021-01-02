7329ca87157ec39aa7140eb7925f03b5c6d0a557
NecroticOoze <blanelysakpsn@gmail.com>
2021-01-01 20:14:22 -0500

    Beginning to reduce my code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
@@ -7,102 +7,95 @@ import Users from "./components/users/Users";
 import User from "./components/users/User";
 import About from "./components/pages/About";
 import axios from "axios";
+import GithubState from './context/github/GithubState';
 
-class App extends Component {
-  state = {
-    users: [],
-    user: {},
-    repos: [],
-    loading: false,
-    alert: null,
-  };
-
-  //  Searches for users
-  searchUsers = async (text) => {
-    this.setState({ loading: true });
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    // console.log(res.data.items);
-    this.setState({ users: res.data.items, loading: false });
-  };
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(null);
 
   //  Get single Github User
-  getUser = async (username) => {
-    this.setState({ loading: true });
+  const getUser = async (username) => {
+    setLoading(true);
+
     const res = await axios.get(
       `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    this.setState({ user: res.data, loading: false });
+
+    setUser(res.data);
+    setLoading(false);
   };
 
   //  Get user repos
-  getUserRepos = async (username) => {
-    this.setState({ loading: true });
+  const getUserRepos = async (username) => {
+    setLoading(true);
+
     const res = await axios.get(
       `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    console.log(res.data);
-    this.setState({ repos: res.data, loading: false });
+
+    setLoading(false);
+    setRepos(res.data);
   };
 
   //  Clears state/page
-  clearUsers = () => this.setState({ users: [], loading: false });
-
-  //  Creates an alert and resets the alert after 5 seconds
-  setAlert = (msg, type) => {
-    this.setState({ alert: { msg, type } });
-    setTimeout(() => {
-      this.setState({ alert: null });
-    }, 5000);
+  const clearUsers = () => {
+    setUsers([]);
+    setLoading(false);
   };
 
-  render() {
-    const { users, loading, user, repos } = this.state;
+  //  Creates an alert and resets the alert after 5 seconds
+  const showAlert = (msg, type) => {
+    setAlert({ msg, type });
 
-    return (
-      <Router>
-        <Fragment>
-          <Navbar />
-          <Alert alert={this.state.alert} />
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={(props) => (
-                <Fragment>
-                  <div className="container my-3">
-                    <Search
-                      searchUsers={this.searchUsers}
-                      clearUsers={this.clearUsers}
-                      showClear={users.length > 0 ? true : false}
-                      setAlert={this.setAlert}
-                    />
-                  </div>
-                  <Users loading={loading} users={users} />
-                </Fragment>
-              )}
-            />
-            <Route exact path="/about" component={About} />
-            <Route
-              exact
-              path="/user/:login"
-              render={(props) => (
-                <User
-                  {...props}
-                  getUser={this.getUser}
-                  getUserRepos={this.getUserRepos}
-                  user={user}
-                  loading={loading}
-                  repos={repos}
-                />
-              )}
-            />
-          </Switch>
-        </Fragment>
-      </Router>
-    );
-  }
-}
+    setTimeout(() => setAlert(null), 5000);
+  };
+
+  return (
+    <GithubState>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Alert alert={alert} />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+              <Fragment>
+                <div className="container my-3">
+                  <Search
+                    clearUsers={clearUsers}
+                    showClear={users.length > 0 ? true : false}
+                    setAlert={showAlert}
+                  />
+                </div>
+                <Users loading={loading} users={users} />
+              </Fragment>
+            )}
+          />
+          <Route exact path="/about" component={About} />
+          <Route
+            exact
+            path="/user/:login"
+            render={(props) => (
+              <User
+                {...props}
+                getUser={getUser}
+                getUserRepos={getUserRepos}
+                user={user}
+                loading={loading}
+                repos={repos}
+              />
+            )}
+          />
+        </Switch>
+      </Fragment>
+    </Router>
+    </GithubState>
+  );
+};
 
 export default App;
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,67 +1,63 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment, useState, useContext } from "react";
+import GithubContext from '../../context/github/githubContext'
 import PropTypes from "prop-types";
 
-class Search extends Component {
-  state = {
-    text: "",
-  };
+const Search = ({ showClear, clearUsers, setAlert }) => {
+  const githubContext = useContext(GithubContext)
 
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired,
-  };
+  const [text, setText] = useState("");
 
-  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+  const onChange = (e) => setText(e.target.value);
 
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    if (this.state.text === "") {
-      this.props.setAlert("Please enter something", "secondary");
+    if (text === "") {
+      setAlert("Please enter something", "secondary");
     } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: "" });
+      githubContext.searchUsers(text);
+      setText("");
     }
   };
 
-  render() {
-    const { showClear, clearUsers } = this.props;
-
-    return (
-      <Fragment>
-        <form onSubmit={this.onSubmit}>
-          <div className="row">
-            <div className="col-md-10 ">
-              <input
-                type="text"
-                name="text"
-                placeholder="Search Users..."
-                className="form-control"
-                value={this.state.text}
-                onChange={this.onChange}
-              />
-            </div>
-            <div className="col-md-2 mt-3 mt-md-0">
-              <button type="search" className="btn btn-dark bg-gradient w-100">
-                Search
-              </button>
-            </div>
+  return (
+    <Fragment>
+      <form onSubmit={onSubmit}>
+        <div className="row">
+          <div className="col-md-10 ">
+            <input
+              type="text"
+              name="text"
+              placeholder="Search Users..."
+              className="form-control"
+              value={text}
+              onChange={onChange}
+            />
           </div>
-        </form>
-        {showClear && (
-          <div className="d-grid gap-2 my-3">
-            <button
-              className="btn btn-secondary bg-gradient"
-              onClick={clearUsers}
-            >
-              Clear
+          <div className="col-md-2 mt-3 mt-md-0">
+            <button type="search" className="btn btn-dark bg-gradient w-100">
+              Search
             </button>
           </div>
-        )}
-      </Fragment>
-    );
-  }
-}
+        </div>
+      </form>
+      {showClear && (
+        <div className="d-grid gap-2 my-3">
+          <button
+            className="btn btn-secondary bg-gradient"
+            onClick={clearUsers}
+          >
+            Clear
+          </button>
+        </div>
+      )}
+    </Fragment>
+  );
+};
+
+Search.propTypes = {
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired,
+};
 
 export default Search;
